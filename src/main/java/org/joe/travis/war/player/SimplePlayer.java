cbe935948419aba41c9b49758a27421b776f42b6
travisj <joe.travis@cdk.com>
2016-03-13 21:00:08 -0700

    Adding player classes and tests

---
+++ src/main/java/org/joe/travis/war/player/SimplePlayer.java
@@ -0,0 +1,53 @@
+package org.joe.travis.war.player;
+
+import org.joe.travis.war.deck.Card;
+
+import java.util.LinkedList;
+
+/**
+ * Simple player implementation with LinkedList for card tracking.
+ */
+public class SimplePlayer implements Player {
+    /**
+     * Player's identifier.
+     */
+    private final int id;
+
+    /**
+     * Cards in player's pile.
+     */
+    private final LinkedList<Card> cards;
+
+    /**
+     * Players need an identifier.
+     * @param id player ID.
+     */
+    public SimplePlayer(final int id) {
+        this.id = id;
+        this.cards = new LinkedList<>();
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public Card play() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+
+        return cards.removeFirst();
+    }
+
+    @Override
+    public void receive(final Card card) {
+        cards.addLast(card);
+    }
+
+    @Override
+    public boolean hasCards() {
+        return !cards.isEmpty();
+    }
+}
---
+++ src/test/java/org/joe/travis/war/player/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Unit tests for player implementations.
+ */
+package org.joe.travis.war.player;
